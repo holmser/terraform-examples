f1309788197771b0c3c3a5e4636412933e0bf7e8
Holmes <chlmes@8c8590906088.ant.amazon.com>
2018-03-13 14:36:49 -0400

    basic bootstrap and vpc

---
+++ vpc/roles.tf
@@ -0,0 +1,145 @@
+# We need to define which region to create the roles
+provider "aws" {
+  region = "us-east-1"
+}
+
+#
+#  IAM role notes:
+#  aws_instance_profile <- iam_role <-
+#
+#
+#
+resource "aws_iam_instance_profile" "cloudwatch_agent_server_profile" {
+  name = "tf_CloudWatchAgentServerProfile"
+  role = "${aws_iam_role.cloudwatch_agent_server_role.name}"
+}
+
+resource "aws_iam_instance_profile" "cloudwatch_agent_admin_profile" {
+  name = "tf_CloudWatchAgentAdminProfile"
+  role = "${aws_iam_role.cloudwatch_agent_admin_role.name}"
+}
+
+resource "aws_iam_role" "cloudwatch_agent_server_role" {
+  name = "tf_CloudWatchAgentServerRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "cloudwatch_agent_server_policy" {
+  name = "tf_CloudWatchAgentServerPolicy"
+  role = "${aws_iam_role.cloudwatch_agent_server_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "CloudWatchAgentServerPolicy",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "cloudwatch:PutMetricData",
+                "ec2:DescribeTags",
+                "logs:DescribeLogStreams",
+                "logs:CreateLogGroup",
+                "logs:PutLogEvents",
+                "ssm:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ssm_agent_policy" {
+  name = "tf_SSMAgentPolicy"
+  role = "${aws_iam_role.cloudwatch_agent_server_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "SSMAgentPolicy",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "cloudwatch:PutMetricData",
+                "ec2:DescribeTags",
+                "logs:DescribeLogStreams",
+                "logs:CreateLogGroup",
+                "logs:PutLogEvents"
+
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "cloudwatch_agent_admin_policy" {
+  name = "tf_CloudWatchAgentServerPolicy"
+  role = "${aws_iam_role.cloudwatch_agent_admin_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "CloudWatchAgentAdminPolicy",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "cloudwatch:PutMetricData",
+                "ec2:DescribeTags",
+                "ec2messages:*",
+                "ssm:UpdateInstanceInformation",
+                "ssm:ListAssociations",
+                "logs:DescribeLogStreams",
+                "logs:CreateLogGroup",
+                "logs:PutLogEvents",
+                "ssm:*",
+                "ssm:PutParameter"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role" "cloudwatch_agent_admin_role" {
+  name = "tf_CloudWatchAgentAdminRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
---
+++ vpc/vars.tf
@@ -0,0 +1,26 @@
+variable "vpc_cidr" {
+  description = "CIDR for the whole VPC"
+  default     = "10.1.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for the Public Subnet"
+  default     = "10.1.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR for the Private Subnet"
+  default     = "10.1.1.0/24"
+}
+
+variable "zones" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
+}
+
+terraform {
+  backend "s3" {
+    bucket = "chlmes.terraform"
+    key    = "tf_state/bootstrap"
+    region = "us-east-1"
+  }
+}
